38e2ce8a56fdfe2f3ca48774bf29f4aec75a4b75
Steen Pinnerup <application.sandbox@gmail.com>
2014-10-27 15:28:03 +0300

    MSF document, initial commit

---
+++ classes/msf.class.php
@@ -0,0 +1,357 @@
+<?php
+include_once ('classes/budget.class.php');
+include_once ('classes/document.class.php');
+include_once ('classes/msf_record.class.php');
+include_once ('../common/eiseGrid/inc_eiseGrid.php');
+include_once ('classes/item.class.php');
+
+$Items = new Items();
+
+$arrJS[] = '../common/eiseGrid/eiseGrid.js';
+$arrCSS[] = '../common/eiseGrid/eiseGrid.css';
+
+class MSF extends Document{
+	
+	const MSF_ITEM = '090d883b-5061-11e4-926a-00155d010e0b';
+	
+	function __construct($id=''){
+		GLOBAL $strLocal;
+		//GLOBAL $arrUsrData;
+		
+		$this->register = 'reg_msf';
+		$this->gridName = 'msf';
+		$this->table = 'tbl_msf';
+		$this->prefix = 'msf';
+		
+		parent::__construct($id);	
+
+	}
+	public function refresh($id){
+		
+		$sqlWhere = $this->getSqlWhere($id);
+		
+		$sql = "SELECT ".$this->prefix.".*, `usrTitle` FROM `".$this->table."` ".$this->prefix."
+				LEFT JOIN `stbl_user` ON `usrID`=`".$this->prefix."EditBy`
+				WHERE $sqlWhere";
+		
+		parent::refresh($sql);		
+				
+		$this->total = $this->data[$this->prefix."Total"];
+		$this->item = self::MSF_ITEM;
+				
+		if($this->GUID){
+			$this->subtotal = Array();
+			$sql = "SELECT * FROM `".$this->register."` WHERE `source`='".$this->GUID."';";
+			$rs = $this->oSQL->q($sql);			
+			while($rw = $this->oSQL->f($rs)){
+				// print_r($rw);
+				$this->records[$this->gridName][$rw['id']] = new msf_record($this->GUID, $this->scenario, $rw['id'], $rw);			
+				for($m=1;$m<13;$m++){
+					$month = strtolower(date('M',mktime(0,0,0,$m,15)));
+					$this->subtotal[$month] += $rw[$month];
+				}				
+			}		
+		}
+	}
+	
+	public function defineEF(){
+	
+		global $arrUsrData;
+		global $budget_scenario;
+		global $Items;
+		
+		parent::defineEF();
+		
+		// $this->Columns[] = Array(
+					// 'title'=>'Item'
+					// ,'field'=>$this->prefix.'ItemGUID'
+					// ,'type'=>'combobox'
+					// ,'sql'=>$Items->getStructuredRef()
+					// , 'mandatory' => true
+					// , 'disabled'=>!$this->flagUpdate
+				// );
+		$this->Columns[] =Array(
+					'title'=>'Total'
+					,'field'=>$this->prefix."Total"
+					,'type'=>'decimal'
+					,'mandatory'=>true
+					, 'disabled'=>!$this->flagUpdate
+				);
+			
+		$this->Columns[] = Array(
+			'title'=>'Comments'
+			,'field'=>$this->prefix.'Comment'
+			,'type'=>'text'
+			, 'disabled'=>!$this->flagUpdate
+		);
+	}
+	
+	public function defineGrid(){
+		
+		GLOBAL $Items;
+	
+		$grid = new eiseGrid($this->oSQL
+                    ,$this->gridName
+                    , Array(
+                            'flagKeepLastRow' => false
+                            , 'arrPermissions' => Array("FlagWrite" => !$this->flagPosted)
+                            , 'flagStandAlone' => true
+							, 'controlBarButtons' => "add"
+                            )
+                    );
+		$grid->Columns[]=Array(
+			'field'=>"id"
+			,'type'=>'row_id'
+		);
+		
+		$grid->Columns[] = Array(
+				'title'=>'PC'
+				,'field'=>'pc'
+				,'type'=>'combobox'
+				,'table'=>'vw_profit'
+				,'source'=>'vw_profit'
+				,'prefix'=>'pcc'
+				,'sql'=>"SELECT pccID as optValue, pccTitle as optText FROM vw_profit"
+				, 'mandatory' => true
+				, 'disabled'=>false
+				, 'default'=>0
+				// , 'class'=>'costs_supplier'
+			);		
+
+		$grid->Columns[] =Array(
+			'title'=>'Unit'
+			,'field'=>'unit'
+			,'type'=>'text'
+			,'mandatory'=>true
+		);
+		
+	
+		for ($m=1;$m<13;$m++){
+			$month = date('M',mktime(0,0,0,$m,15));
+					
+			$grid->Columns[] = Array(
+			'title'=>$month
+			,'field'=>strtolower($month)
+			,'class'=>'budget-month'
+			,'type'=>'decimal'
+			, 'mandatory' => true
+			, 'disabled'=>false
+			,'totals'=>true
+		);
+		}
+		$grid->Columns[] =Array(
+			'title'=>'Total'
+			,'field'=>'YTD'
+			,'type'=>'decimal'
+			,'totals'=>true
+			,'disabled'=>true
+		);
+		
+		$this->grid = $grid;
+		return ($grid);
+	}
+	
+
+	
+	public function add_record(){		
+		$oBR = new msf_record($this->GUID,$this->scenario);
+		$this->records[$this->gridName][] = $oBR;
+		return ($oBR);	
+	}
+	
+	public function save($mode='update'){
+		
+		GLOBAL $arrUsrData;
+		GLOBAL $Activities;
+		GLOBAL $YACT;
+		GLOBAL $Items;
+		
+		if (!$this->ID){
+			$this->Update();
+			return(true);
+		}
+		
+		//echo '<pre>';print_r($_POST);die('</pre>');
+		if($mode=='update' || $mode=='post'){			
+			$this->comment = isset($_POST[$this->prefix.'Comment'])?$_POST[$this->prefix.'Comment']:$this->comment;
+			$this->scenario = isset($_POST[$this->prefix.'Scenario'])?$_POST[$this->prefix.'Scenario']:$this->scenario;
+			$this->profit = isset($_POST[$this->prefix.'ProfitID'])?$_POST[$this->prefix.'ProfitID']:$this->profit;
+			$this->item = isset($_POST[$this->prefix.'ItemGUID'])?$_POST[$this->prefix.'ItemGUID']:$this->item;
+			
+			$sql = "SELECT SUM(".Budget::getYTDSQL().") as Total, ".Budget::getMonthlySumSQL()." 
+						FROM reg_master 
+						WHERE active=1 AND scenario='{$this->scenario}' AND pc={$this->profit}";
+			$rs = $this->oSQL->q($sql);	
+			$total = $this->oSQL->f($rs);
+			
+			$this->total = -$total['Total'];
+		}
+		//-------------------Updating grid records---------------------------------
+		$arrUpdated = $_POST['inp_'.$this->gridName.'_updated'];
+		if(is_array($arrUpdated)){
+			$nRows = count($_POST['inp_'.$this->gridName.'_updated']);
+			for($id=1;$id<$nRows;$id++){		
+
+				$row = $this->get_record($_POST['id'][$id]);					
+
+				if ($row){
+					if ($arrUpdated[$id]){				
+						$row->flagUpdated = true;				
+						$row->pc = isset($_POST['pc'][$id]) ? $_POST['pc'][$id] : $this->profit;						
+						$row->unit = $_POST['unit'][$id];					
+						for ($m=1;$m<13;$m++){
+							$month = date('M',mktime(0,0,0,$m,15));
+							$row->{$month} = (double)$_POST[strtolower($month)][$id];
+						}					
+					} else {
+						$row->flagUpdated = false;
+					}
+				}
+			}	
+		}
+		
+		$this->deleteGridRecords();
+		
+		$settings = Budget::getSettings($this->oSQL,$this->scenario);
+		// echo '<pre>';print_r($settings);echo '</pre>';			
+		
+		$sql = Array();
+		$sql[] = "SET AUTOCOMMIT = 0;";
+		$sql[] = "START TRANSACTION;";
+		$sql[] = "UPDATE `".$this->table."` 
+						SET ".$this->prefix."ProfitID=".$this->profit."
+						,".$this->prefix."Total=".(double)$this->total."						
+						,".$this->prefix."Comment=".$this->oSQL->e($this->comment)."
+						,".$this->prefix."Scenario='".$this->scenario."'
+						,".$this->prefix."EditBy='".$arrUsrData['usrID']."'
+						,".$this->prefix."EditDate=NOW()
+						WHERE ".$this->prefix."ID={$this->ID};";
+
+		if(is_array($this->records[$this->gridName])){			
+			foreach ($this->records[$this->gridName] as $i=>$row){				
+				if ($row->flagUpdated || $row->flagDeleted){
+					$sql[] = $row->getSQLstring();
+				}
+			}
+		};
+		
+		$sql[] = "SET AUTOCOMMIT = 1;";
+		$sql[] = 'COMMIT;';				
+		//echo '<pre>';print_r($sql);echo '</pre>';die();
+		$sqlSuccess = $this->doSQL($sql);
+		
+		if ($mode=='delete'){
+			$this->delete();
+		}
+		
+		if ($mode=='unpost'){
+			$this->unpost();
+		}
+		
+		if($mode=='post'){
+			$this->refresh($this->ID);//echo '<pre>',print_r($this->data);echo '</pre>';
+			$oMaster = new budget_session($this->scenario, $this->GUID);
+			// print_r($this->subtotal);
+			if(is_array($this->records[$this->gridName])){
+					// echo '<pre>';print_r($this->records[$this->gridName]);echo '</pre>';die();
+					foreach($this->records[$this->gridName] as $id=>$record){
+
+							$master_row = $oMaster->add_master();
+							$master_row->profit = $record->pc;
+							// $master_row->activity = $total['activity'];
+							// $master_row->customer = $record->customer;					
+							$item = $Items->getById($this->item);
+							//$master_row->account = $item->getYACT($this->profit);
+							$master_row->item = $this->item;
+							for($m=1;$m<13;$m++){
+								$month = date('M',mktime(0,0,0,$m,15));
+								$master_row->{$month} = $record->{$month}/$this->subtotal[strtolower($month)]*$total[$month];
+							}				
+													
+							
+							//echo '<pre>';print_r($master_row);echo '</pre>';
+		
+					}
+					
+					$master_row = $oMaster->add_master();
+					$master_row->profit = $this->profit;
+					// $master_row->activity = $total['activity'];
+					// $master_row->customer = self::EMPTY_CUSTOMER;					
+					$item = $Items->getById($this->item);
+					//$master_row->account = $item->getYACT($this->profit);
+					$master_row->item = $this->item;
+					for($m=1;$m<13;$m++){
+						$month = date('M',mktime(0,0,0,$m,15));
+						$master_row->{$month} = -$total[$month];
+					}
+				
+				$oMaster->save();
+				$this->markPosted();
+			}
+		}
+		
+		return($sqlSuccess);
+				
+	}
+	
+	public function fill_distribution($oBudget,$type='sqm',$params=Array()){
+		
+		if (is_array($this->records[$this->gridName])){
+			foreach($this->records[$this->gridName] as $id=>$record){
+				$record->flagDeleted = true;
+			}
+		}
+		
+		switch ($type) {
+			case 'fte':
+				$sql = "SELECT 'FTE' as unit, pc, ".Budget::getMonthlySumSQL()." FROM reg_headcount
+						LEFT JOIN vw_profit ON pccID=pc
+						WHERE scenario='".$oBudget->id."' 
+							AND posted=1 
+							AND pc<>'{$this->profit}'
+							AND pccFlagProd=1
+						GROUP BY pc"; 
+			break;
+			case 'sales':
+				$sql = "SELECT 'RUB' as unit, pc, ".Budget::getMonthlySumSQL()." FROM reg_master
+						LEFT JOIN vw_profit ON pccID=pc
+						WHERE scenario='".$oBudget->id."' 
+							AND active=1 
+							AND pc<>'{$this->profit}'
+							AND pccFlagProd=1 AND pc<>99
+							AND account='J00400'
+						GROUP BY pc"; 
+			break;
+			case 'users':
+				$sql = "SELECT pc, wc, activity, 'user' as unit, ".Budget::getMonthlySumSQL()." FROM reg_headcount
+					LEFT JOIN vw_profit ON pccID=pc				
+					WHERE scenario='".$oBudget->id."' 
+						AND posted=1 AND wc=1 
+						AND pccFlagProd=1 AND pc<>99
+						GROUP BY pc, activity";
+				break;
+			default:
+				return (false);
+			break;
+		}
+		 	
+		$rs = $this->oSQL->q($sql);
+		while ($rw=$this->oSQL->f($rs)){
+			$row = $this->add_record();
+			$row->flagUpdated = true;				
+			$row->unit = $rw['unit'];
+			$row->pc = $rw['pc'];			
+			for ($m=1;$m<13;$m++){
+				$month = date('M',mktime(0,0,0,$m,15));				
+				$row->set_month_value($m, $rw[$month]);
+				$arrSubtotal[$month] += $rw[$month];
+				$arrSum[$month] = $this->total - $arrSubtotal[$month];
+			}
+		}
+		
+	}
+	
+}
+
+?>
+
---
+++ msf_form.php
@@ -0,0 +1,101 @@
+<?php
+//$flagNoAuth = true;
+require ('common/auth.php');
+include ('classes/msf.class.php');
+$arrJS[] = 'js/input_form.js';
+
+$msfID=$_GET['msfID']?$_GET['msfID']:$_POST['msfID'];
+
+$oBudget = new Budget($budget_scenario);
+$oDocument = new MSF($msfID);
+$oDocument->defineEF();
+$oDocument->defineGrid();
+
+
+if ($_POST['DataAction']){
+	 // echo '<pre>'; print_r($_POST);	 echo '</pre>';
+	
+	switch ($_POST['DataAction']){
+		case 'fill_fte':
+			$oDocument->fill_distribution($oBudget, 'fte');
+			break;
+		case 'fill_sales':
+			$oDocument->fill_distribution($oBudget, 'sales');
+			break;	
+		case 'fill_users':
+			$oDocument->fill_distribution($oBudget, 'users');
+			break;			
+	}
+	
+	if ($oDocument->save($_POST['DataAction'])){
+		$oDocument->refresh($oDocument->ID);
+		
+		$arrActions = Array();
+		include('includes/inc_document_menu.php');
+		$oDocument->arrActions = $arrActions;
+		$oDocument->status = 'success';
+		header("Content-type: application/json");
+		$oDocument->getJSON();
+	}
+	die();
+}
+
+if ($_GET['tab']){
+	switch($_GET['tab']){
+		case 'kpi':
+			require_once ('classes/reports.class.php');
+			$sqlWhere = "WHERE source='".$oDocument->GUID."'";
+			Reports::costsBySupplier($sqlWhere);
+			die();
+			break;
+		case 'financials':
+			require_once ('classes/reports.class.php');
+			$sqlWhere= "WHERE source='".$oDocument->GUID."'";			
+			Reports::masterByProfit($sqlWhere);			
+			Reports::masterByYACT($sqlWhere);
+			die();
+			break;
+		default:
+			break;
+	}
+
+}
+
+
+include ('includes/inc_document_menu.php');
+if ($oDocument->GUID && !$oDocument->flagPosted){
+	$arrActions[] = Array ('title'=>'FTE','action'=>'javascript:fillGrid(\'_fte\');','class'=>'brick');	
+	$arrActions[] = Array ('title'=>'Users','action'=>'javascript:fillGrid(\'_users\');','class'=>'brick');	
+	$arrActions[] = Array ('title'=>'Sales','action'=>'javascript:fillGrid(\'_sales\');','class'=>'brick');	
+}
+
+//============================== Main form definition ==============================
+
+$oDocument->fillGrid($oDocument->grid);
+
+require ('includes/inc-frame_top.php');
+require ('includes/inc_document_header.php');
+
+?>
+<script>
+var arrGHQ = <?php echo json_encode($arrGHQ);?>;
+
+$(document).ready(function(){
+	eiseGridInitialize();
+	rowTotalsInitialize();
+	
+	var grid=eiseGrid_find(doc.gridName);
+    if (grid!=null){  
+		grid.change ("jan[]", function(oTr, input){
+			for (m=1;m<months.length;m++){
+				oTr.find("input[name='"+months[m]+"[]']").val(input.val());
+			}
+		})
+    }
+	
+});
+
+</script>
+<?php
+require ('includes/inc-frame_bottom.php');
+?>
