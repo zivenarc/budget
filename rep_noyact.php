e3d2278e6ce81bdef85dc31e240363ea5ab9eca2
Steen Pinnerup <application.sandbox@gmail.com>
2014-10-29 17:53:11 +0300

    New functions

--- rep_noyact.php
+++ rep_noyact.php
@@ -18,6 +18,25 @@ if ($_GET['tab']){
 		while ($rw=$oSQL->f($rs)){
 			$data[] = $rw;
 		}
+		echo '<h3>Missing YACT</h3>';
+		Reports::getJournalEntries($data);
+		
+		$data = Array();
+		
+		$sql = "SELECT *, edit_date as timestamp FROM vw_master 		
+		LEFT JOIN vw_journal ON source=guid
+		LEFT JOIN stbl_user ON usrID=edit_by
+		LEFT JOIN vw_profit ON pccID=vw_master.pc
+		##LEFT JOIN vw_location ON locID=vw_journal.pc	
+		WHERE posted=1 AND vw_master.scenario='{$_GET['tab']}' AND IFNULL(pc,0) = 0
+		GROUP BY guid
+		ORDER BY vw_master.timestamp DESC";	
+
+		$rs =$oSQL->q($sql);
+		while ($rw=$oSQL->f($rs)){
+			$data[] = $rw;
+		}
+		echo '<h3>Missing PC</h3>';
 		Reports::getJournalEntries($data);
 		
 	
