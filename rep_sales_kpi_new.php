6d399de1221e0a66ab9ca752743279b8cb35d242
Steen Pinnerup <application.sandbox@gmail.com>
2014-12-08 15:54:20 +0300

    Select for customers

--- rep_sales_kpi_new.php
+++ rep_sales_kpi_new.php
@@ -3,6 +3,7 @@ $flagNoAuth = true;
 require ('common/auth.php');
 require ('classes/budget.class.php');
 
+$cntID = isset($_GET['cntID'])?(integer)$_GET['cntID']:9907;//new if not defined
 $budget_scenario = isset($_GET['budget_scenario'])?$_GET['budget_scenario']:$budget_scenario;
 
 $oBudget = new Budget($budget_scenario);
@@ -13,6 +14,12 @@ if(!isset($_GET['tab'])){
 	$arrJS[]='js/rep_pnl.js';
 	include ('includes/inc-frame_top.php');
 	echo '<h1>',$oBudget->title,' :: ',$arrUsrData["pagTitle$strLocal"],'</h1>';
+	
+	$sql = "SELECT cntTitle$strLocal FROM vw_customer WHERE cntID={$cntID} LIMIT 1";
+	$rs = $oSQL->q($sql);
+	$cntTitle = $oSQL->get_data($rs);
+	echo '<h2>',$cntTitle,'</h2>';
+	
 	?>
 	<div class='f-row'><label for='budget_scenario'>Select scenario</label><?php echo Budget::getScenarioSelect();?></div>
 	<?php
@@ -22,9 +29,9 @@ if(!isset($_GET['tab'])){
 	require ('classes/reports.class.php');
 	include ('includes/inc_report_buttons.php');
 	if ($_GET['tab']=='all'){
-		$sqlWhere = " WHERE scenario='$budget_scenario' AND customer=9907";
+		$sqlWhere = " WHERE scenario='$budget_scenario' AND customer={$cntID}";
 	} else {
-		$sqlWhere = "WHERE pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") AND scenario='$budget_scenario' AND customer=9907";
+		$sqlWhere = "WHERE pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") AND scenario='$budget_scenario' AND customer={$cntID}";
 	}
 
 	Reports::salesByActivity($sqlWhere);
