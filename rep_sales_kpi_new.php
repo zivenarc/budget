44fe8aeeece4093e93c926c874205236c425a180
Steen Pinnerup <application.sandbox@gmail.com>
2014-12-08 16:06:50 +0300

    fdfsdg

--- rep_sales_kpi_new.php
+++ rep_sales_kpi_new.php
@@ -3,15 +3,13 @@ $flagNoAuth = true;
 require ('common/auth.php');
 require ('classes/budget.class.php');
 
-unset($_SESSION['cntID']);
-
-$cntID = isset($_GET['cntID'])?(integer)$_GET['cntID']:9907;//new if not defined
-$budget_scenario = isset($_GET['budget_scenario'])?$_GET['budget_scenario']:$budget_scenario;
-
-$oBudget = new Budget($budget_scenario);
-
 session_start();
+
+$cntID = isset($_GET['cntID'])?(integer)$_GET['cntID']:(isset($_SESSION['cntID'])?$_SESSION['cntID']:9907);//new if not defined
 $_SESSION['cntID'] = $cntID;
+
+$budget_scenario = isset($_GET['budget_scenario'])?$_GET['budget_scenario']:$budget_scenario;
+$oBudget = new Budget($budget_scenario);
 
 if(!isset($_GET['tab'])){
 	$arrJS[] = 'https://www.google.com/jsapi';
@@ -19,7 +17,7 @@ if(!isset($_GET['tab'])){
 	include ('includes/inc-frame_top.php');
 	echo '<h1>',$oBudget->title,' :: ',$arrUsrData["pagTitle$strLocal"],'</h1>';
 	
-	$sql = "SELECT cntTitle$strLocal FROM vw_customer WHERE cntID={$_SESSION['cntID']} LIMIT 1";
+	$sql = "SELECT cntTitle$strLocal FROM vw_customer WHERE cntID={$cntID} LIMIT 1";
 	$rs = $oSQL->q($sql);
 	$cntTitle = $oSQL->get_data($rs);
 	echo '<h2>',$cntTitle,'</h2>';
@@ -33,9 +31,9 @@ if(!isset($_GET['tab'])){
 	require ('classes/reports.class.php');
 	include ('includes/inc_report_buttons.php');
 	if ($_GET['tab']=='all'){
-		$sqlWhere = " WHERE scenario='$budget_scenario' AND customer={$_SESSION['cntID']}";
+		$sqlWhere = " WHERE scenario='$budget_scenario' AND customer={$cntID}";
 	} else {
-		$sqlWhere = "WHERE pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") AND scenario='$budget_scenario' AND customer={$_SESSION['cntID']}";
+		$sqlWhere = "WHERE pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") AND scenario='$budget_scenario' AND customer={$cntID}";
 	}
 
 	Reports::salesByActivity($sqlWhere);
