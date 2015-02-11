267a233e1b0dade8c3182cbb2c2d2e4bf04c7067
Steen Pinnerup <application.sandbox@gmail.com>
2015-02-11 12:32:27 +0300

    bUGFIX

--- classes/msf.class.php
+++ classes/msf.class.php
@@ -182,7 +182,7 @@ class MSF extends Document{
 			
 			$sql = "SELECT SUM(".Budget::getYTDSQL().") as Total, ".Budget::getMonthlySumSQL()." 
 						FROM reg_master 
-						WHERE active=1 AND scenario='{$this->scenario}' AND pc={$this->profit} AND source NOT IN ('Estimate', 'Actual')";
+						WHERE active=1 AND scenario='{$this->scenario}' AND pc={$this->profit} AND source NOT IN ('Estimate')";
 			$rs = $this->oSQL->q($sql);	
 			$total = $this->oSQL->f($rs);
 			
