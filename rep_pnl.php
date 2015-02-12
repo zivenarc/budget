4f174a999063dddf5d5540121c07299fad57d101
Steen Pinnerup <application.sandbox@gmail.com>
2015-02-12 10:29:43 +0300

    Added detail type

--- rep_pnl.php
+++ rep_pnl.php
@@ -5,10 +5,40 @@ require ('classes/budget.class.php');
 $budget_scenario = isset($_GET['budget_scenario'])?$_GET['budget_scenario']:$budget_scenario;
 $currency = isset($_GET['currency'])?$_GET['currency']:643;
 
+//------------------------------------- type of report details -------------------------------------------//
+if (isset($_GET['type'])){
+	$type = $_GET['type'];
+	SetCookie('pnl_type',$type,0,'/budget/');
+} elseif (isset($_COOKIE['pnl_type'])) {
+	$type = $_COOKIE['pnl_type'];
+} else {
+	switch ($_GET['tab']){
+		case 'f865db6b-d328-102e-9d25-5de97ba9df63':
+		case 'f865e1de-d328-102e-9d25-5de97ba9df63':
+		case '48b5ae6c-e650-11de-959c-00188bc729d2':
+			$type = 'activity';			
+			break;
+		case 'all':
+			$type = 'ghq';			
+		break;
+		case 'f865e855-d328-102e-9d25-5de97ba9df63':
+			$type = 'customer';
+			//$sqlWhere = "WHERE (pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") OR (customer=9907 AND Group_code=94))";
+		default:
+			$type = 'customer';			
+			break;
+	}
+}
+
 if(!isset($_GET['tab'])){
 	$oBudget = new Budget($budget_scenario);
 	$arrJS[]='js/rep_pnl.js';
 	// $arrJS[]='js/input_form.js';	
+	
+	$arrActions[] = Array ('title'=>'By customer','action'=>"?budget_scenario={$budget_scenario}&type=customer");
+	$arrActions[] = Array ('title'=>'By activity','action'=>"?budget_scenario={$budget_scenario}&type=activity");
+	$arrActions[] = Array ('title'=>'By GHQ type','action'=>"?budget_scenario={$budget_scenario}&type=ghq");
+		
 	include ('includes/inc-frame_top.php');
 	echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
 	echo '<p>',$oBudget->timestamp,'; ',$oBudget->rates,'</p>';
@@ -34,18 +64,17 @@ if(!isset($_GET['tab'])){
 	} else {
 		$sqlWhere = "WHERE pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).")";
 	}
-	switch ($_GET['tab']){
-		case 'f865db6b-d328-102e-9d25-5de97ba9df63':
-		case 'f865e1de-d328-102e-9d25-5de97ba9df63':
-		case '48b5ae6c-e650-11de-959c-00188bc729d2':
+	switch ($type){
+		case 'activity':		
 			Reports::masterByActivityEst($sqlWhere." AND scenario='$budget_scenario'",$currency);	
 			break;
-		case 'all':
+		case 'ghq':
 			echo "<input type='hidden' id='group' value='activity'/>";
 			Reports::masterByGHQEst($sqlWhere." AND scenario='$budget_scenario'",$currency);	
 		break;
-		case 'f865e855-d328-102e-9d25-5de97ba9df63':
-			$sqlWhere = "WHERE (pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") OR (customer=9907 AND Group_code=94))";
+		// case 'f865e855-d328-102e-9d25-5de97ba9df63':
+			// $sqlWhere = "WHERE (pc in (SELECT pccID FROM vw_profit WHERE pccGUID=".$oSQL->e($_GET['tab']).") OR (customer=9907 AND Group_code=94))";
+		case 'customer':
 		default:
 			echo "<input type='hidden' id='group' value='customer'/>";
 			Reports::masterByCustomerEst($sqlWhere." AND scenario='$budget_scenario'",$currency);
