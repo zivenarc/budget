e4f8538ab503e566ff592b6aed384225d1301ef5
Steen Pinnerup <application.sandbox@gmail.com>
2015-07-23 10:35:07 +0300

    Deer's mistake

--- classes/reports.class.php
+++ classes/reports.class.php
@@ -802,6 +802,10 @@ class Reports{
 				<tr>
 					<th><?php echo $firstLevelTitle; ?></th>
 					<th>Account</th>					
+					<?php 
+						echo $Budget->getTableHeader();
+						echo $Budget->getTableHeader('quarterly');
+					?>		
 					<th class='budget-ytd'><?php echo $Budget->type=='Budget'?'Budget':'FYE';?></th>
 					<th><?php echo $Budget->type=='Budget'?'Estimate':'Budget';?></th>
 					<th>Diff</th>
