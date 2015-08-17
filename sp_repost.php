fc91c9823af8f99a1b955798ae61a923f7fe8ca3
Steen Pinnerup <application.sandbox@gmail.com>
2015-08-17 10:59:57 +0300

    Post filter off

--- sp_repost.php
+++ sp_repost.php
@@ -14,7 +14,8 @@ if ($_GET['tab']){
 		LEFT JOIN stbl_user ON usrID=vw_journal.edit_by		
 		LEFT JOIN vw_profit ON pccID=vw_journal.pc
 		LEFT JOIN vw_location ON locID=vw_journal.pc
-		WHERE vw_journal.posted=1 AND vw_journal.scenario='{$_GET['tab']}' 
+		##WHERE vw_journal.posted=1 AND vw_journal.scenario='{$_GET['tab']}' 
+		WHERE vw_journal.scenario='{$_GET['tab']}' 
 		AND guid IN (SELECT DISTINCT source FROM reg_sales WHERE selling_curr<>'RUB' OR buying_curr<>'RUB' 
 						UNION ALL 
 					SELECT DISTINCT source FROM reg_costs WHERE buying_curr<>'RUB')
