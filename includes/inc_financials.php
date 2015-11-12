4ccf63c3387db92c2b03eb8edc105a5f9b46f0ef
Steen Pinnerup <application.sandbox@gmail.com>
2015-11-12 10:36:17 +0300

    Bugfix

--- includes/inc_financials.php
+++ includes/inc_financials.php
@@ -1,7 +1,8 @@
 <?php
 	require_once ('classes/reports.class.php');
-	$sqlWhere= "WHERE source='".$oDocument->GUID."'";			
-	Reports::masterByActivity($sqlWhere);
-	Reports::masterByYACT($sqlWhere);
+	$sqlWhere= "WHERE source='".$oDocument->GUID."'";	
+	$oReport = new Reports(Array('budget_scenario'=>$oDocument->budget->id));
+	$oReport->masterByActivity($sqlWhere);
+	$oReport->masterByYACT($sqlWhere);
 	die();
 ?>
