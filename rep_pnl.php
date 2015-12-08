b0eccd0237b55009a30664f93e3e003ee903b496
Steen Pinnerup <application.sandbox@gmail.com>
2015-12-08 09:56:45 +0300

    Selector buttons

--- rep_pnl.php
+++ rep_pnl.php
@@ -4,11 +4,28 @@ require ('classes/budget.class.php');
 
 include ('includes/inc_report_settings.php');
 
+if($currency!=643){
+		$sql = "SELECT * FROM vw_currency WHERE curID={$currency} LIMIT 1";
+		$rs = $oSQL->q($sql);
+		$rw = $oSQL->f($rs);
+		$curTitle = $rw["curTitle$strLocal"];		
+} else {
+	$curTitle = "RUB";
+}
+
+$arrUsrData["pagTitle$strLocal"] .= ': '.$curTitle;
+
+if ($denominator!=1) {
+	$arrUsrData["pagTitle$strLocal"] .= ' x'.$denominator;
+}
+
+
 if(!isset($_GET['pccGUID'])){
 	$oBudget = new Budget($budget_scenario);
 	$arrJS[]='js/rep_pnl.js';
 	// $arrJS[]='js/input_form.js';	
 	
+	$arrActions[] = Array ('title'=>'By customer group','action'=>"?type=customer_group");
 	$arrActions[] = Array ('title'=>'By customer','action'=>"?type=customer");
 	$arrActions[] = Array ('title'=>'By activity','action'=>"?type=activity");
 	$arrActions[] = Array ('title'=>'By GHQ type','action'=>"?type=ghq");
@@ -17,22 +34,14 @@ if(!isset($_GET['pccGUID'])){
 		
 	include ('includes/inc-frame_top.php');
 	echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
+	include ('includes/inc_report_selectors.php');
 	echo '<p>',$oBudget->timestamp,'; ',$oBudget->rates,'</p>';
-	?>
-	<div class='f-row'><label for='budget_scenario'>Select scenario</label><?php echo Budget::getScenarioSelect();?></div>
-	<?php
+	
 	Budget::getProfitTabs('reg_master', true);
 	include ('includes/inc-frame_bottom.php');
 } else {
 	require ('classes/reports.class.php');
 	include ('includes/inc_report_buttons.php');
-	
-	if(isset($currency)){
-		$sql = "SELECT * FROM vw_currency WHERE curID={$currency} LIMIT 1";
-		$rs = $oSQL->q($sql);
-		$rw = $oSQL->f($rs);
-		echo '<h2>',$rw["curTitle$strLocal"],'</h2>';
-	}
 	
 	if ($_GET['pccGUID']=='all'){
 		$strRoles = "'".implode("','",$arrUsrData['roleIDs'])."'";
@@ -68,8 +77,11 @@ if(!isset($_GET['pccGUID'])){
 			$oReport->periodicPnL($sqlWhere,Array('field_data'=>'pc','field_title'=>'Profit','title'=>'PC'));	
 			break;
 		case 'customer':
+			$oReport->periodicPnL($sqlWhere,Array('field_data'=>'customer','field_title'=>'Customer_name','title'=>'Customer'));
+			break;
+		case 'customer_group':
 		default:			
-			$oReport->periodicPnL($sqlWhere,Array('field_data'=>'CASE WHEN Customer_group_code=723 THEN customer ELSE Customer_group_code END','field_title'=>'CASE WHEN Customer_group_code=723 THEN Customer_name ELSE Customer_group_title END','title'=>'Customer'));
+			$oReport->periodicPnL($sqlWhere,Array('field_data'=>'CASE WHEN Customer_group_code=723 THEN customer ELSE Customer_group_code END','field_title'=>'CASE WHEN Customer_group_code=723 THEN Customer_name ELSE Customer_group_title END','title'=>'Customer group'));
 			break;
 	}
 
