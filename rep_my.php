5675d9391314446e9232fcfbd3c20b195c2895ca
Steen Pinnerup <application.sandbox@gmail.com>
2016-04-11 16:11:55 +0300

    OwnerID

--- rep_my.php
+++ rep_my.php
@@ -3,6 +3,14 @@
 // $arrUsrData['usrID'] = 'ZHAROVA';
 
 require ('common/auth.php');
+
+
+$ownerID = isset($_GET['ownerID'])?$_GET['ownerID']:($_COOKIE['ownerID']?$_COOKIE['ownerID']:$arrUsrData['usrID']);
+if ($_GET['ownerID']=='MYSELF'){
+	$ownerID = $arrUsrData['usrID'];
+}
+SetCookie('ownerID',$ownerID,0);
+
 require ('classes/budget.class.php');
 require ('classes/reports.class.php');
 include ('includes/inc_report_settings.php');
@@ -15,8 +23,6 @@ $arrActions[] = Array ('title'=>'By customer','action'=>"?type=customer");
 $arrActions[] = Array ('title'=>'By activity','action'=>"?type=activity");
 $arrActions[] = Array ('title'=>'By GHQ type','action'=>"?type=ghq");
 
-$usrID = isset($_GET['usrID'])?$_GET['usrID']:$arrUsrData['usrID'];
-
 if(isset($currency)){
 		$sql = "SELECT * FROM vw_currency WHERE curID={$currency} LIMIT 1";
 		$rs = $oSQL->q($sql);
@@ -24,7 +30,7 @@ if(isset($currency)){
 		echo '<h2>',$rw["curTitle$strLocal"],'</h2>';
 }
 
-$sql = "SELECT * FROM stbl_user WHERE usrID=".$oSQL->e($usrID);
+$sql = "SELECT * FROM stbl_user WHERE usrID=".$oSQL->e($ownerID);
 $rs = $oSQL->q($sql);
 $rw = $oSQL->f($rs);
 $arrUsrData["pagTitle$strLocal"] = 'Sales by '.($rw['usrTitle']?$rw['usrTitle']:'<Unknown>');
@@ -36,7 +42,7 @@ echo '<p>',$oBudget->timestamp,'; ',$oBudget->rates,'</p>';
 ?>
 <div class='f-row'><label for='budget_scenario'>Select scenario</label><?php echo Budget::getScenarioSelect();?></div>
 <?php
-$sqlWhere .= "WHERE scenario='$budget_scenario' AND sales = ".$oSQL->e($usrID);
+$sqlWhere .= "WHERE scenario='$budget_scenario' AND sales = ".$oSQL->e($ownerID);
 $oReport = new Reports(Array('budget_scenario'=>$budget_scenario, 'currency'=>$currency, 'denominator'=>$denominator));
 	
 switch ($type){
@@ -54,7 +60,7 @@ switch ($type){
 ?>
 <h2>KPI</h2>
 <?php
-$oReport->salesByCustomer(' and sales='.$oSQL->e($usrID));
+$oReport->salesByCustomer(' and sales='.$oSQL->e($ownerID));
 
 include ('includes/inc-frame_bottom.php');
 ?>
