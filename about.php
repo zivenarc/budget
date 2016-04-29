737776919538e165f5d0ea45ca95f7e69d9f2ccb
Steen Pinnerup <application.sandbox@gmail.com>
2016-04-29 17:55:57 +0300

    Waterfall

--- about.php
+++ about.php
@@ -2,6 +2,7 @@
 require ('common/auth.php');
 require ('classes/budget.class.php');
 require ('classes/reports.class.php');
+require ('classes/waterfall.class.php');
 
    $arrActions[] = Array(
    "title" => ShowFieldTitle('Print')
@@ -17,6 +18,7 @@ require ('classes/reports.class.php');
 $arrJS[]='js/about.js';
 require ('includes/inc-frame_top.php');
 
+$denominator = 1000;
 $oBudget = new Budget($arrSetup['stpFYEID']);
 $oReference = new Budget($arrSetup['stpScenarioID']);
 ?>
@@ -29,8 +31,42 @@ $oReference = new Budget($arrSetup['stpScenarioID']);
 <br/>
 <?php
 
-$oReport = new Reports(Array('budget_scenario'=>$oBudget->id, 'currency'=>643, 'denominator'=>1000, 'reference'=>$oReference->id, 'filter'=>$filter));
+$oReport = new Reports(Array('budget_scenario'=>$oBudget->id, 'currency'=>643, 'denominator'=>$denominator, 'reference'=>$oReference->id, 'filter'=>$filter));
 $oReport->shortMonthlyReport();	
 
+//-----------------Waterfall
+?>
+<div>
+<?php
+$period_type = 'cm';
+$sqlActual = "SUM(".$oBudget->getThisYTDSQL($period_type,$arrActualRates).")/{$denominator}";
+$sqlBudget = "SUM(".$oBudget->getThisYTDSQL($period_type,$arrBudgetRates).")/{$denominator}";
+$settings['gpcus'] = Array('title'=>"GP by customer",
+					'sqlBase' => "SELECT IF(C.cntParentID<>723,C.cntParentID, C.cntID) as optValue, 
+										IF(C.cntParentID<>723,(SELECT P.cntTitle FROM common_db.tbl_counterparty P WHERE P.cntID=C.cntParentID),cntTitle) as optText, 
+										{$sqlActual} as Actual, 
+										0 as Budget, 
+										{$sqlActual} as Diff
+								FROM vw_master 
+								LEFT JOIN common_db.tbl_counterparty C ON C.cntID=customer
+								WHERE scenario='{$oBudget->id}' AND account IN ('J00400', 'J00802')
+								GROUP BY IF(C.cntParentID<>723,C.cntParentID, C.cntID)
+								UNION ALL
+								SELECT IF(C.cntParentID<>723,C.cntParentID,C.cntID), IF(C.cntParentID<>723,(SELECT P.cntTitle FROM common_db.tbl_counterparty P WHERE P.cntID=C.cntParentID),cntTitle) as optText, 0 as Actual, 
+								{$sqlBudget}  as Budget, -{$sqlBudget} as Diff
+								FROM vw_master 
+								LEFT JOIN common_db.tbl_counterparty C ON C.cntID=customer
+								WHERE
+								scenario='{$oReference->id}' AND source<>'Estimate' AND account IN ('J00400', 'J00802')
+								GROUP BY IF(C.cntParentID<>723,C.cntParentID, C.cntID)",
+						'tolerance'=>0.05,
+						'limit'=>10);	
+
+$oWF = new Waterfall($settings['gpcus']);
+
+$oWF->draw();
+?>
+</div>
+<?php
 require ('includes/inc-frame_bottom.php');
 ?>
