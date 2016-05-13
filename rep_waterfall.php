929d54fd051170e7075ba743b4fff12c5696746d
Steen Pinnerup <application.sandbox@gmail.com>
2016-05-13 10:04:23 +0300

    Next month added

--- rep_waterfall.php
+++ rep_waterfall.php
@@ -31,10 +31,12 @@ SetCookie('period_type',$period_type,0,'/budget/');
 
 $arrPeriodType['ytd'] = 'YTD';
 $arrPeriodType['cm'] = 'Current month';
+$arrPeriodType['nm'] = 'Next month';
 $arrPeriodType['q1'] = '1<sup>st</sup> quarter';
 $arrPeriodType['q2'] = '2<sup>nd</sup> quarter';
 $arrPeriodType['q3'] = '3<sup>rd</sup> quarter';
 $arrPeriodType['q4'] = '4<sup>th</sup> quarter';
+$arrPeriodType['q5'] = '5<sup>th</sup> quarter';
 $arrPeriodType['roy'] = 'Rest-of-year';
 $arrPeriodType['fye'] = 'FYE';
 $arrPeriodType['am'] = 'Apr-Mar';
