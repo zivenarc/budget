adfd3f7b66d1717286a31981315b3e86c3fd040c
Steen Pinnerup <application.sandbox@gmail.com>
2016-06-14 14:09:22 +0300

    dm

--- rep_sales_kpi_new.php
+++ rep_sales_kpi_new.php
@@ -45,6 +45,7 @@ function getCnt($cntID, &$arrCounterparty=Array()){
 
 if(!isset($_GET['pccGUID'])){
 
+	$arrActions[] = Array ('title'=>'By customer','action'=>"?type=customer");
 	$arrActions[] = Array ('title'=>'By activity','action'=>"?type=activity");
 	$arrActions[] = Array ('title'=>'By GHQ type','action'=>"?type=ghq");
 	$arrActions[] = Array ('title'=>'By BDV staff','action'=>"?type=sales");	
