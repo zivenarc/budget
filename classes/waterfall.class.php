8719ca864064e6d6e8ba3f6d368c0199591eec47
Steen Pinnerup <application.sandbox@gmail.com>
2016-06-24 16:54:05 +0300

    New library

--- classes/waterfall.class.php
+++ classes/waterfall.class.php
@@ -1,6 +1,11 @@
 <?php
-$arrJS[] = 'https://www.google.com/jsapi';
-$arrJS[] = 'js/waterfall.js';
+// $arrJS[] = 'https://www.google.com/jsapi';
+//$arrJS[] = 'js/waterfall.js';
+
+$arrJS[]="https://code.highcharts.com/highcharts.js";
+$arrJS[]="https://code.highcharts.com/highcharts-more.js";
+$arrJS[]="https://code.highcharts.com/modules/exporting.js";
+
 class Waterfall {
 	
 	private $arrReport, $arrChart, $chartID;
@@ -12,12 +17,16 @@ class Waterfall {
 		$this->chartID = md5($this->sqlBase);
 		$this->title = $options['title'];
 		$this->tolerance = $options['tolerance']?$options['tolerance']:0.1;
+		$this->currency = $options['currency']?$options['currency']:'RUB';
+		$this->actual_title = $options['actual_title']?$options['actual_title']:'Actual';
+		$this->budget_title = $options['budget_title']?$options['budget_title']:'Budget';
 	}
 	
 	private function _initData(){
 	
 		$this->arrReport = Array();
 		$this->arrChart = Array();
+		$this->arrHSChart = Array();
 	
 		GLOBAL $oSQL;
 		$sql = "SELECT SUM(Actual) as Actual, SUM(Budget) as Budget, SUM(Diff) as Diff FROM 
@@ -27,6 +36,7 @@ class Waterfall {
 		$baseData = $oSQL->f($rs);
 		$this->arrReport[] = Array('Budget',null,null,$baseData['Budget'], 'budget-subtotal');
 		$this->arrChart[] = Array('Budget',0,0,(integer)$baseData['Budget'],(integer)$baseData['Budget'], $this->getTooltip('Budget',$baseData['Budget']));
+		$this->arrHSChart[] = Array('name'=>$this->budget_title,'y'=>(integer)$baseData['Budget'], 'color'=>'blue');
 		$diffBalance = $baseData['Diff'];
 		$thisBalance = $baseData['Actual'];
 		$thatBalance = $baseData['Budget'];
@@ -75,6 +85,7 @@ class Waterfall {
 					// $this->arrReport[] = Array($rw['optText'],$rw['Diff']);
 					$this->arrReport[] = Array($rw['optText'],$rw['Actual'],$rw['Budget'],$rw['Diff']);
 					$this->arrChart[] = Array($rw['optText'],(integer)$lastValue,(integer)$lastValue,(integer)($lastValue+$rw['Diff']),(integer)($lastValue+$rw['Diff']), $strTooltip);
+					$this->arrHSChart[] = Array('name'=>$rw['optText'],'y'=>(integer)$rw['Diff']);
 					$lastValue += $rw['Diff'];
 					$diffBalance -= $rw['Diff'];
 					$thisBalance -= $rw['Actual'];
@@ -87,9 +98,11 @@ class Waterfall {
 			$this->arrReport[] = Array('Other',$thisBalance, $thatBalance, $diffBalance);
 			$this->arrChart[] = Array('Other',(integer)$lastValue,(integer)$lastValue, (integer)($lastValue+$diffBalance), (integer)($lastValue+$diffBalance)
 							, $this->getTooltip('Other factors not included before',$diffBalance));
+			$this->arrHSChart[] = Array('name'=>$rw['optText'],'y'=>(integer)$diffBalance);
 		}
 		$this->arrReport[] = Array('Actual',null, null, $baseData['Actual'],'budget-subtotal');
 		$this->arrChart[] = Array('Actual',0,0,(integer)$baseData['Actual'],(integer)$baseData['Actual'], $this->getTooltip('Actual',$baseData['Actual']));
+		$this->arrHSChart[] = Array('name'=>$this->actual_title,'y'=>(integer)$baseData['Actual'],'isSum'=>true, 'color'=>'blue');
 		
 		$this->arrReport[] = Array('Total diff',null, null, $baseData['Diff'],'budget-subtotal');
 		$this->arrReport[] = Array('Ratio',null, null, $baseData['Actual']/$baseData['Budget']*100,'budget-ratio');
@@ -101,13 +114,21 @@ class Waterfall {
 		<h2><?php echo $this->title;?></h2>
 		<p>Tolerance = <?php echo ($this->tolerance*100).'%';?></p>
 		<table id="table_<?php echo $this->chartID;?>" class="<?php echo $strClass;?>" style="width:auto;">
+		<thead>
+			<tr>
+				<th>Factors</th>			
+				<th><?php echo $this->actual_title;?></th>			
+				<th><?php echo $this->budget_title;?></th>			
+				<th>Diff</th>			
+			</tr>
+		</thead>
 		<?php
 		foreach($this->arrReport as $record){
 			?>
 			<tr class="<?php echo $record[4];?>">
 				<td><?php echo $record[0];?></td>
-				<td class='budget-decimal'><span style='color:<?php echo $record[1]<0?'red':'black';?>;'><?php echo number_format($record[1],0,'.',',');?></span></td>
-				<td class='budget-decimal'><span style='color:<?php echo $record[2]<0?'red':'black';?>;'><?php echo number_format($record[2],0,'.',',');?></span></td>
+				<td class='budget-decimal'><span style='color:<?php echo $record[1]<0?'red':'black';?>;'><?php echo $record[1]==null?'&nbsp;':number_format($record[1],0,'.',',');?></span></td>
+				<td class='budget-decimal'><span style='color:<?php echo $record[2]<0?'red':'black';?>;'><?php echo $record[2]==null?'&nbsp;':number_format($record[2],0,'.',',');?></span></td>
 				<td class='budget-decimal'><span style='color:<?php echo $record[3]<0?'red':'black';?>;'><?php echo number_format($record[3],0,'.',',');?></span></td>
 			</tr>
 			<?php
@@ -126,10 +147,36 @@ class Waterfall {
 		<div id="<?php echo $this->chartID;?>" class="google_chart" style="width: 1200px; height: 700px;"></div>
 		<div id="toolbar_div"></div>
 		<script>
+			var hs_data = [];
+			
+			//google_chart_data["<?php echo $this->chartID;?>"] = {data:<?php echo json_encode($this->arrChart);?>,title:'<?php echo $this->title;?>'};			
+			hs_data["<?php echo $this->chartID;?>"] = {chart: {type: 'waterfall'},
+													title: {text: '<?php echo $this->title;?>'},
+													xAxis: {type: 'category'},
+													yAxis: {title: {
+																text: '<?php echo $this->currency;?>'
+															}},
+													legend: {enabled: false},
+													tooltip: {pointFormat: '<b>{point.y:,.2f}</b> <?php echo $this->currency;?>'},
+													series: [{upColor: 'green',
+															color: 'red',
+													data: <?php echo json_encode($this->arrHSChart);?>,
+													dataLabels: {
+														enabled: true,
+														formatter: function () {
+															return Highcharts.numberFormat(this.y, 0, ',');
+														},
+													style: {
+														color: '#FFFFFF',
+														fontWeight: 'bold',
+														textShadow: '0px 0px 3px black'
+														}
+													},
+													pointPadding: 0
+												}]};
 				
-			google_chart_data["<?php echo $this->chartID;?>"] = {data:<?php echo json_encode($this->arrChart);?>,title:'<?php echo $this->title;?>'};
-			// google_chart_data["<?php echo $chartID;?>"]['title'] = '<?php echo $this->title;?>';
-				  
+				$('#<?php echo $this->chartID;?>').highcharts(hs_data["<?php echo $this->chartID;?>"]);
+			
 			</script>
 		<?php
 	}
