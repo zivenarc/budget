0069bc38457d907e4f0f628e50d55bef5eaa4bbc
Steen Pinnerup <application.sandbox@gmail.com>
2016-09-20 10:37:14 +0300

    BO

--- sp_get_kpi.php
+++ sp_get_kpi.php
@@ -47,8 +47,8 @@ for ($i=0; $i<count($arrKPI);$i++){
 		$month = $oBudget->arrPeriod[$m];
 
 		$sql[] = "SET @dateStart:='{$repDateStart}', @dateEnd:='{$repDateEnd}'";
-		$sql[] = "INSERT INTO budget.reg_sales (pc,activity,unit,customer,`{$month}`,source,scenario,active,posted,kpi,sales, bdv)
-					SELECT jobProfitID, @prtID, @unit, cntID, {$arrKPI[$i]['kpi']} as '{$month}', 'Actual', @scenario, 1,1,1, cntUserID, usrProfitID
+		$sql[] = "INSERT INTO budget.reg_sales (pc,activity,unit,customer,`{$month}`,source,scenario,active,posted,kpi,sales, bdv, bo)
+					SELECT jobProfitID, @prtID, @unit, cntID, {$arrKPI[$i]['kpi']} as '{$month}', 'Actual', @scenario, 1,1,1, cntUserID, usrProfitID, jobGDSBusinessOwnerID
 					FROM nlogjc.tbl_job
 					JOIN common_db.tbl_counterparty ON cntID=jobCustomerID
 					JOIN common_db.stbl_user ON usrID=cntUserID
