57abaeeaf0503011cb13c50eef41ddbb8b849ccd
Steen Pinnerup <application.sandbox@gmail.com>
2016-10-11 14:50:46 +0300

    Filter for PC

--- sp_my.php
+++ sp_my.php
@@ -8,7 +8,32 @@ if ($_GET['ownerID']=='MYSELF'){
 	$ownerID = $arrUsrData['usrID'];
 }
 SetCookie('ownerID',$ownerID,0);
+$sqlWhere = " AND responsible=".$oSQL->e($ownerID);
 
+$sql = "SELECT * FROM stbl_user WHERE usrID=".$oSQL->e($ownerID);
+$rs = $oSQL->q($sql);
+$rw = $oSQL->f($rs);
+$arrUsrData["pagTitle$strLocal"] .= ' :: '.($rw['usrTitle']?$rw['usrTitle']:'<Unknown>');
+
+if(isset($_GET['ownerID'])){
+	SetCookie('pc',null);
+	unset($pcfilter);
+}
+
+if(isset($_GET['pc'])){
+	$pcfilter = isset($_GET['pc'])?$_GET['pc']:($_COOKIE['pc']?$_COOKIE['pc']:$arrUsrData['usrProfitID']);
+	SetCookie('pc',$pcfilter,0);
+	
+	$sql = "SELECT * FROM vw_profit WHERE pccID=".$oSQL->e($pcfilter);
+	$rs = $oSQL->q($sql);
+	$rw = $oSQL->f($rs);
+	$arrUsrData["pagTitle$strLocal"] .= ' :: '.($rw['pccTitle']?$rw['pccTitle']:'<Unknown PC>');
+}
+
+if ($_COOKIE['pc']){
+	$pcfilter = $_COOKIE['pc'];
+	$sqlWhere = " AND guid IN (SELECT source FROM reg_master WHERE pc=".$oSQL->e($pcfilter).")";
+}
 
 if ($_GET['tab']){
 	
@@ -21,7 +46,7 @@ if ($_GET['tab']){
 	$sql = "SELECT vw_journal.*, stbl_user.*, edit_date as timestamp FROM vw_journal 				
 		LEFT JOIN stbl_user ON usrID=vw_journal.edit_by				
 		WHERE vw_journal.scenario='{$_GET['tab']}' 
-		AND responsible=".$oSQL->e($ownerID)."
+		{$sqlWhere}
 		GROUP BY vw_journal.guid
 		ORDER BY vw_journal.edit_date ASC";	
 
@@ -38,12 +63,6 @@ if ($_GET['tab']){
 } else {
 	require ('classes/budget.class.php');
 	$arrJS[] = 'js/journal.js';
-
-	
-	$sql = "SELECT * FROM stbl_user WHERE usrID=".$oSQL->e($ownerID);
-	$rs = $oSQL->q($sql);
-	$rw = $oSQL->f($rs);
-	$arrUsrData["pagTitle$strLocal"] .= ' :: '.($rw['usrTitle']?$rw['usrTitle']:'<Unknown>');
 	
 	include ('includes/inc-frame_top.php');
 	echo '<h1>',$arrUsrData["pagTitle$strLocal"],'</h1>';
