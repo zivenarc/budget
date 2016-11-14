037d34bbf933c75f1f7773cdb400a65ac7963561
Steen Pinnerup <application.sandbox@gmail.com>
2016-11-14 15:32:44 +0300

    Full data

--- rep_hr_costs.php
+++ rep_hr_costs.php
@@ -12,7 +12,33 @@ $arrJS[]='js/rep_pnl.js';
 include ('includes/inc-frame_top.php');
 echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
 
-$sqlWhere = "WHERE `Group_code` IN (95)  AND scenario='$budget_scenario'";
+if($_GET['DataAction']=='full'){
+	$sql = "SELECT * FROM vw_headcount WHERE scenario='{$budget_scenario}' ORDER BY empTitleLocal";
+	$rs = $oSQL->q($sql);
+	?>
+	<table id='headcount' class='log'>
+	<?php
+	while ($rw = $oSQL->f($rs)){
+		?>
+		<tr>
+			<td><?php echo $rw['empCode1C'];?></td>
+			<td><?php echo $rw['empTitleLocal'];?></td>
+			<td><?php echo $rw['funTitleLocal'];?></td>
+			<td><?php echo $rw['Location'];?></td>
+			<td><?php echo $rw['pccTitle'];?></td>
+			<td><?php echo $rw['empSalary'];?></td>
+			<td><?php echo $rw['salary'];?></td>
+		</tr>
+		<?php
+	}
+	?>
+	</table>
+	<?php
+	include ('includes/inc-frame_bottom.php');
+	die();
+}
+
+$sqlWhere = "WHERE `Group_code` IN (95)  AND scenario='{$budget_scenario}'";
 ?>
 <script>
 	$(document).ready(function(){
