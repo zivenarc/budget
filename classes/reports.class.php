58d0ba856b0188a216eea83c0017c630c7432b0d
Steen Pinnerup <application.sandbox@gmail.com>
2016-11-17 13:12:00 +0300

    Buntick

--- classes/reports.class.php
+++ classes/reports.class.php
@@ -219,7 +219,7 @@ class Reports{
 				break;
 		}
 		
-		$sql = "SELECT activity, route, rteTitle, prtTitle, ".$this->oBudget->getMonthlySumSQL(1,15).", 
+		$sql = "SELECT activity, route, rteTitle, prtGHQ, ".$this->oBudget->getMonthlySumSQL(1,15).", 
 				SUM(".$this->oBudget->getYTDSQL(1+$this->oBudget->offset,12+$this->oBudget->offset).") as Total
 				FROM 
 					({$sqlFrom}) U 		
@@ -259,7 +259,7 @@ class Reports{
 					<tr><th colspan="20"><?php echo $rw['prtGHQ'];?></th></tr>
 					<?php
 				};
-				echo "<td><a href='javascript:getCustomerKPI({activity:{$rw['activity']},route:{$rw['route']} ,freehand:true});'>",$rw['prtTitle']," | ",$rw['rteTitle'],'</a></td>';								
+				echo "<td><a href='javascript:getCustomerKPI({activity:{$rw['activity']},route:{$rw['route']} ,freehand:true});'>",$rw['prtGHQ']," | ",$rw['rteTitle'],'</a></td>';								
 				
 				for ($m=1+$this->oBudget->offset;$m<=12+$this->oBudget->offset;$m++){
 					// $month = $this->oBudget->arrPeriod[$m];
