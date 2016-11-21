9b107dbf9c732894da01b89e59ce31360312ecde
Steen Pinnerup <application.sandbox@gmail.com>
2016-11-21 15:58:26 +0300

    DM

--- rep_sales_kpi_new.php
+++ rep_sales_kpi_new.php
@@ -105,8 +105,7 @@ if(!isset($_GET['pccGUID'])){
 		$sql = "SELECT vw_journal.*, stbl_user.*, edit_date as timestamp FROM vw_journal 				
 		LEFT JOIN stbl_user ON usrID=vw_journal.edit_by				
 		WHERE vw_journal.scenario='{$budget_scenario}' 
-			AND vw_journal.customer IN (".implode(',',$arrCounterparty['codes']).")
-		{$sqlWhere}
+			AND vw_journal.customer IN (".implode(',',$arrCounterparty['codes']).")		
 		GROUP BY vw_journal.guid
 		ORDER BY vw_journal.edit_date ASC";	
 
