d55339e021b0bf4069143fb145c86d3d1a3f361f
Steen Pinnerup <application.sandbox@gmail.com>
2016-12-23 16:07:58 +0300

    DM

--- interco_sales_form.php
+++ interco_sales_form.php
@@ -34,7 +34,8 @@ if ($_GET['tab']){
 		case 'kpi':
 			require_once ('classes/reports.class.php');
 			$sqlWhere = "WHERE source='".$oDocument->GUID."'";
-			Reports::salesByActivity($sqlWhere);
+			$oReport = new Reports(Array('budget_scenario'=>$oDocument->budget->id));
+			$oReport->salesByActivity($sqlWhere);
 			die();
 			break;
 		case 'financials':
