e9444e417ba460dec23630a4c96d07621b5a9b83
Steen Pinnerup <application.sandbox@gmail.com>
2016-12-28 13:22:43 +0300

    Tolerance for GP breakdown

--- rep_waterfall.php
+++ rep_waterfall.php
@@ -122,7 +122,7 @@ $settings['gpbu'] = Array('title'=>"GP by business unit",
 			WHERE
 			scenario='{$budget}' AND source<>'Estimate' ".Reports::GP_FILTER."
 			GROUP BY pc",
-			'tolerance'=>0.00
+			'tolerance'=>0.001
 			);
 
 $settings['scbu'] = Array('title'=>"Staff cost by business unit",
