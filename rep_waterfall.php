509eb38e343cd1350e797615dcb9a119e1a7070a
Steen Pinnerup <application.sandbox@gmail.com>
2017-04-13 11:04:40 +0300

    No tolerance on Staff costs

--- rep_waterfall.php
+++ rep_waterfall.php
@@ -159,7 +159,7 @@ $settings['scbu'] = Array('title'=>"Staff cost by business unit",
 			WHERE
 			scenario='{$budget}' AND source<>'Estimate' ".Reports::SC_FILTER." AND company='{$company}'
 			GROUP BY pc",
-			'tolerance'=>0.05
+			'tolerance'=>0.00
 			);			
 			
 $settings['gpact'] = Array('title'=>"GP by activity",
