604cafde760e33652e7d5eef3ecf37a42cc2c503
Steen Pinnerup <application.sandbox@gmail.com>
2017-05-30 16:10:43 +0300

    DM

--- classes/reports.class.php
+++ classes/reports.class.php
@@ -2782,7 +2782,7 @@ class Reports{
 				?>
 				<td class='budget-decimal budget-ytd'><?php self::render($data['FYE_A'],0);?></td>
 				<td class='budget-decimal'><?php self::render($data['FYE_B'],0);?></td>
-				<td class='budget-decimal'><?php self::render($data['FYE_A']-$data['Q_B'],0);?></td>
+				<td class='budget-decimal'><?php self::render($data['FYE_A']-$data['FYE_B'],0);?></td>
 				<td class='budget-decimal'><em><?php self::render_ratio($data['FYE_A'],$data['FYE_B']);?></em></td>
 				<?php
 				}
