a7c87bfc0b8a925bad9785b493a9b0c0da0a86bf
Steen Pinnerup <application.sandbox@gmail.com>
2017-06-07 10:45:15 +0300

    Syntax DM!

--- sp_get_kpi.php
+++ sp_get_kpi.php
@@ -196,7 +196,7 @@ $sql[] = "INSERT IGNORE INTO ref_route
 $sql[] = "UPDATE reg_sales, ref_route SET reg_sales.route=ref_route.route WHERE LEFT(pol,2)=pol_country AND LEFT(pod,2)=pod_country AND scenario=@scenario";
 $sql[] = "UPDATE reg_sales SET freehand=1 WHERE bo=714 and jo<>714 AND activity=48 AND scenario=@scenario";
 $sql[] = "UPDATE reg_sales SET freehand=1 WHERE bo=714 and jo=714 AND activity=63 AND scenario=@scenario";
-$sql[] = "update reg_sales set customer_group_code=common_db.fn_parentl2(customer) where customer is not null and scenario=@scenario_new;"
+$sql[] = "update reg_sales set customer_group_code=common_db.fn_parentl2(customer) where customer is not null and scenario=@scenario_new;";
 
 
 for ($i=0;$i<count($sql);$i++){	
