dbcc73c94a24136b3ee8ee6cc813a4b00cb6e235
Steen Pinnerup <application.sandbox@gmail.com>
2017-07-10 13:41:58 +0300

    Moar

--- budget_methods.php
+++ budget_methods.php
@@ -12,6 +12,14 @@ echo '<pre>',$oBudget->getMonthlySQL(1,15),'</pre>';
 echo '<pre>',$oBudget->getMonthlySumSQL(1,12),'</pre>';
 echo '<pre>',$oBudget->getMonthlySumSQL(4,15),'</pre>';
 echo '<pre>',$oBudget->getQuarterlySumSQL(),'</pre>';
+?>
+<h1>Budget methods</h1>
+<?php
+echo '<pre>'; print_r(get_class_methods('Budget')); echo '</pre>';
+?>
+<h1>Budget variables</h1>
+<?php
+echo '<pre>'; print_r(get_class_vars('Budget')); echo '</pre>';
 
 include ('includes/inc-frame_bottom.php');
 
