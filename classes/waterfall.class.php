a9e20a1ee4bd903f25c4cb9c5dab619b0145bb8e
Steen Pinnerup <application.sandbox@gmail.com>
2017-08-02 09:31:58 +0300

    DM?

--- classes/waterfall.class.php
+++ classes/waterfall.class.php
@@ -280,7 +280,7 @@ class Waterfall {
 							for(i=0;i<data.table.length;i++){
 								var tr = $('<tr>',{'class':data.table[i][4]}).appendTo(datatable);
 								$('<td>',{html:data.table[i][0]}).appendTo(tr);
-								for(j=1;j<4;j++){
+								for(var j=1;j<4;j++){
 									var strNumber = (data.table[i][j] == null) ? '' : number_format(data.table[i][j],0,'.',',');
 									if (data.table[i][j]<0) {
 										strNumber = '<span class="budget-negative">'+strNumber+'</span>';
