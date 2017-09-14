73a8b869e316b413d5c338bb0c6678e0f2bebddd
Joseph Topotushko <jt@usa.net>
2017-09-14 18:42:06 +0300

    Игтешслы

--- rep_customer_group.php
+++ rep_customer_group.php
@@ -1,5 +1,5 @@
 <?php
-$flagNoAuth = true;
+// $flagNoAuth = true;
 require ('common/auth.php');
 require ('classes/budget.class.php');
 
@@ -8,9 +8,13 @@ $oBudget = new Budget($budget_scenario);
 $sql = "SELECT customer_group_code, cntTitle, SUM(".$oBudget->getThisYTDSQL('ytd').") as YTD 
 		FROM reg_master 
 		LEFT JOIN common_db.tbl_counterparty ON cntID=customer_group_code
-		WHERE account IN ('J00400','J00802') AND scenario IN ('{$arrSetup['stpFYEID']}','{$arrSetup['stpScenarioID']}')
+		WHERE account IN ('J00400','J00802') 
+			AND scenario IN ('{$arrSetup['stpFYEID']}','{$arrSetup['stpScenarioID']}') 
+			AND company={$company}
+			AND (pc='{$arrUsrData['usrID']}' OR bdv='{$arrUsrData['usrID']}')
 		GROUP BY customer_group_code		
 		ORDER BY YTD DESC
+		LIMIT 50
 		";
 $rs = $oSQL->q($sql);
 while ($rw = $oSQL->f($rs)){
@@ -26,7 +30,7 @@ include ('includes/inc-frame_top.php');
 $(document).ready(function(){
 	
 	var width = $('body').css('width');
-	var height = $('body').css('height');
+	var height = $('body').css('height')/2;
 	$('#cloud').css({'width':width,'height':height});
 
 	var words = <?php echo json_encode($arrJQCloud); ?>;
@@ -35,7 +39,7 @@ $(document).ready(function(){
 	  autoResize: true,
 	  fontSize: {
 		from: 6,
-		to: 48
+		to: 72
 	  }	  
 	});
 });
