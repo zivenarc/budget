10055b2baca023f00213ba1a33ebb3c5236f38dd
Joseph Topotushko <jt@usa.net>
2017-10-02 08:57:41 +0300

    Debug

--- msf_form.php
+++ msf_form.php
@@ -26,7 +26,7 @@ if ($_POST['DataAction']){
 			$oDocument->fill_distribution($oBudget, 'net_sales');
 			break;	
 		case 'fill_net_sales_last':
-			$oDocument->fill_distribution($oBudget->reference, 'net_sales');
+			echo $oDocument->fill_distribution($oBudget->reference, 'net_sales');
 			break;	
 		case 'fill_users':
 			$oDocument->fill_distribution($oBudget, 'users');
