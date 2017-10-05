e6ea4bfe19b5ab98fae679444b7e47caa71e0108
Joseph Topotushko <jt@usa.net>
2017-10-05 08:31:47 +0300

    Bunticks

--- index.php
+++ index.php
@@ -33,11 +33,11 @@ if (isset($_GET['no_activity'])){
 
 
 $denominator = 1000;
-$oBudget = new Budget($arrSetup['stpFYEID']);
-$oReference = new Budget($arrSetup['stpScenarioID']);
+$oActual = new Budget($arrSetup['stpFYEID']);
+$oBudget = new Budget($arrSetup['stpScenarioID']);
 
 if($_GET['DataAction']=='summary'){
-	$oReport = new Reports(Array('budget_scenario'=>$oBudget->id, 'currency'=>643, 'denominator'=>$denominator, 'reference'=>$oReference->id, 'filter'=>$filter));
+	$oReport = new Reports(Array('budget_scenario'=>$oActual->id, 'currency'=>643, 'denominator'=>$denominator, 'reference'=>$oBudget->id, 'filter'=>$filter));
 	$oReport->shortMonthlyReport();	
 	die();
 }
@@ -50,8 +50,16 @@ $arrDefaultParams = Array('currency'=>643,'period_type'=>'cm','denominator'=>100
 $strQuery = http_build_query($arrDefaultParams);
 
 ?>
-<h1>Current scenario - <?php echo $oBudget->title; ?>, current budget - <?php echo $oReference->title; ?></h1>
+<h1>Current scenario - <?php echo $oActual->title; ?>, current budget - <?php echo $oBudget->title; ?></h1>
 <?php
+if($oActual->flagUpdate){
+	$sql = "SELECT COUNT(guid) as nCount FROM vw_journal WHERE scenario='{$oActual->id}' AND responsible='{$arrUsrData['usrID']}' AND posted=0 AND deleted=0";
+	$rs = $oSQL->q($sql);
+	if($oSQL->n($rs)){
+		$rw = $oSQL->f($rs);
+		echo "<div class='warning'>You have {$rw['nCount']} <a href='sp_my.php?ownerID={$arrUsrData['usrID']}'>unposted documents</a> in [{$oActual->title}]</div>";
+	}
+}
 if($oBudget->flagUpdate){
 	$sql = "SELECT COUNT(guid) as nCount FROM vw_journal WHERE scenario='{$oBudget->id}' AND responsible='{$arrUsrData['usrID']}' AND posted=0 AND deleted=0";
 	$rs = $oSQL->q($sql);
@@ -59,35 +67,29 @@ if($oBudget->flagUpdate){
 		$rw = $oSQL->f($rs);
 		echo "<div class='warning'>You have {$rw['nCount']} <a href='sp_my.php?ownerID={$arrUsrData['usrID']}'>unposted documents</a> in [{$oBudget->title}]</div>";
 	}
+	
+	?>
+	<nav><span><?php echo $oBudget->title;?>: </span>
+		<a href="rep_summary.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Summary</a>|
+		<a href="rep_monthly.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Monthly report</a>|
+		<a href="rep_pnl.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Full-year estimate</a>|
+		<a href="rep_totals.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Results per BU</a>|
+		<a href="rep_waterfall.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Waterfall</a>|
+		<a href="rep_graphs.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Charts</a>
+	</nav>
+	<?php
 }
-if($oReference->flagUpdate){
-	$sql = "SELECT COUNT(guid) as nCount FROM vw_journal WHERE scenario='{$oReference->id}' AND responsible='{$arrUsrData['usrID']}' AND posted=0 AND deleted=0";
-	$rs = $oSQL->q($sql);
-	if($oSQL->n($rs)){
-		$rw = $oSQL->f($rs);
-		echo "<div class='warning'>You have {$rw['nCount']} <a href='sp_my.php?ownerID={$arrUsrData['usrID']}'>unposted documents</a> in [{$oReference->title}]</div>";
-	}
-}
+
 ?>
-<nav><span>vs <?php echo $oReference->title;?>: </span>
-	<a href="rep_summary.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Summary</a>|
-	<a href="rep_monthly.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Monthly report</a>|
-	<a href="rep_pnl.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Full-year estimate</a>|
-	<a href="rep_totals.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Results per BU</a>|
-	<a href="rep_waterfall.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Waterfall</a>|
-	<a href="rep_graphs.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oReference->id;?>">Charts</a>
+<nav><span><?php echo $oActual->title;?> vs <?php echo $oActual->reference_scenario->title;?>:</span>
+	<a href="rep_summary.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Summary</a>|
+	<a href="rep_monthly.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Monthly report</a>|
+	<a href="rep_pnl.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Full-year estimate</a>|
+	<a href="rep_totals.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Results per BU</a>|
+	<a href="rep_waterfall.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Waterfall</a>|
+	<a href="rep_graphs.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oActual->id;?>&reference=<?php echo $oActual->reference_scenario->id;?>">Charts</a>
 </nav>
 
-<?php if ($oReference->id!=$oBudget->reference_scenario->id) { ?>
-<nav><span>vs <?php echo $oBudget->reference_scenario->title;?>:</span>
-	<a href="rep_summary.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Summary</a>|
-	<a href="rep_monthly.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Monthly report</a>|
-	<a href="rep_pnl.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Full-year estimate</a>|
-	<a href="rep_totals.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Results per BU</a>|
-	<a href="rep_waterfall.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Waterfall</a>|
-	<a href="rep_graphs.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $oBudget->id;?>&reference=<?php echo $oBudget->reference_scenario->id;?>">Charts</a>
-</nav>
-<?php } ?>
 <br/>
 <?php
 if ($strActivity) {
@@ -108,8 +110,8 @@ if ($strActivity) {
 <?php
 $period_type = 'cm';
 
-$sqlActual = "SUM(".$oBudget->getThisYTDSQL($period_type,$arrActualRates).")";
-$sqlBudget = "SUM(".$oBudget->getThisYTDSQL($period_type,$arrBudgetRates).")";
+$sqlActual = "SUM(".$oActual->getThisYTDSQL($period_type,$arrActualRates).")";
+$sqlBudget = "SUM(".$oActual->getThisYTDSQL($period_type,$arrBudgetRates).")";
 $settings['gpcus'] = Array('title'=>"GP by customer, current month",
 					'sqlBase' => "SELECT customer_group_code as optValue, 
 											customer_group_title as optText,  
@@ -117,7 +119,7 @@ $settings['gpcus'] = Array('title'=>"GP by customer, current month",
 										0 as Budget, 
 										{$sqlActual} as Diff
 								FROM vw_master 
-								WHERE scenario='{$oBudget->id}'  AND company='{$company}'
+								WHERE scenario='{$oActual->id}'  AND company='{$company}'
 									AND account IN ('J00400', 'J00802')
 									{$sqlActivityFilter}
 								GROUP BY customer_group_code
@@ -127,7 +129,7 @@ $settings['gpcus'] = Array('title'=>"GP by customer, current month",
 											0 as Actual, 
 								{$sqlBudget}  as Budget, -{$sqlBudget} as Diff
 								FROM vw_master 
-								WHERE scenario='{$oReference->id}'  AND company='{$company}'
+								WHERE scenario='{$oBudget->id}'  AND company='{$company}'
 									AND source<>'Estimate' 
 									AND account IN ('J00400', 'J00802')
 									{$sqlActivityFilter}
