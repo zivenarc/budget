9cbcee81e0e6fb9c6713f01a9e33b6e2dbf4d987
Joseph Topotushko <jt@usa.net>
2017-11-08 16:04:24 +0300

    Bunticks

--- rep_customer_pie.php
+++ rep_customer_pie.php
@@ -29,47 +29,97 @@ if ($yOthers){
 	$arrHSSeries[] = Array('name'=>'Others','y'=>$yOthers);
 }
 
+$sql = "SELECT ivlGroup, SUM(".$oBudget->getThisYTDSQL('ytd').") as YTD 
+		FROM reg_master 
+		LEFT JOIN common_db.tbl_counterparty ON cntID=customer_group_code
+		LEFT JOIN common_db.tbl_industry ON cntIndustryID=ivlGUID
+		WHERE account IN ('J00400','J00802') 
+			AND scenario IN ('{$budget_scenario}') 
+			AND company={$company}
+			AND (pc='{$arrUsrData['usrProfitID']}')
+		GROUP BY ivlGroup		
+		ORDER BY YTD DESC
+		";
+$rs = $oSQL->q($sql);
+while ($rw = $oSQL->f($rs)){
+	$arrIndustrySeries[] = Array('name'=>($rw['ivlGroup']?$rw['ivlGroup']:'Unspecified'),'y'=>(integer)$rw['YTD']);
+}
+
 $arrJS[]="https://code.highcharts.com/highcharts.js";
 $arrJS[]="https://code.highcharts.com/highcharts-more.js";
 $arrJS[]="https://code.highcharts.com/modules/exporting.js";
 include ('includes/inc-frame_top.php');
 ?>
-<div id='pie' style='width:1900px; height: 900px;'></div>
+<div id='pie' style='width:1600px; height: 900px;'></div>
+<div id='industry' style='width:1600px; height: 900px;'></div>
 <script>
 $(document).ready(function(){
 	
 	Highcharts.chart('pie', {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: 'Customer share by GP, <?php echo $oBudget->title;?>'
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                style: {
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                }
-            }
-        }
-    },
-    series: [{
-        name: 'Customers',
-        colorByPoint: true,
-        data: <?php echo json_encode($arrHSSeries);?>
-    }]
-});
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false,
+			type: 'pie'
+		},
+		title: {
+			text: 'Customer share by GP, <?php echo $oBudget->title;?>'
+		},
+		tooltip: {
+			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					}
+				}
+			}
+		},
+		series: [{
+			name: 'Customers',
+			colorByPoint: true,
+			data: <?php echo json_encode($arrHSSeries);?>
+		}]
+	});
+	
+	Highcharts.chart('industry', {
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false,
+			type: 'pie'
+		},
+		title: {
+			text: 'Industry verticals by GP, <?php echo $oBudget->title;?>'
+		},
+		tooltip: {
+			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					}
+				}
+			}
+		},
+		series: [{
+			name: 'Industry',
+			colorByPoint: true,
+			data: <?php echo json_encode($arrIndustrySeries);?>
+		}]
+	});
 });
 </script>
 <?php
