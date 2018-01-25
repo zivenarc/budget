b806f37f202224f45f735032bb87be6755b54eb0
Joseph Topotushko <jt@usa.net>
2018-01-25 12:00:20 +0300

    New info

--- rep_fa_unaccounted.php
+++ rep_fa_unaccounted.php
@@ -9,6 +9,14 @@ if(isset($_GET['tab'])){
 	$dateBudgetStart = date('Y-m-d',$oBudget->date_start);
 	$dateBudgetEnd = date('Y-m-d 23:59:59',$oBudget->date_end);
 			
+
+	
+	?>
+	<div id="report">
+	<pre style="display:none;"><?php echo $sql;?></pre>
+	<h2>Unaccounted assets</h2>
+	<ol>
+	<?php
 	$sql = "SELECT *, PERIOD_DIFF(EXTRACT(YEAR_MONTH FROM '{$dateBudgetStart}'), EXTRACT(YEAR_MONTH FROM fixDeprStart)) AS months,
 					fixValueStart*(1-PERIOD_DIFF(EXTRACT(YEAR_MONTH FROM '{$dateBudgetStart}'), EXTRACT(YEAR_MONTH FROM fixDeprStart))/fixDeprDuration) as fixValuePrimo,
 					fixValueStart*(1-PERIOD_DIFF(EXTRACT(YEAR_MONTH FROM '{$dateBudgetEnd}'), EXTRACT(YEAR_MONTH FROM fixDeprStart))/fixDeprDuration) as fixValueUltimo
@@ -23,15 +31,30 @@ if(isset($_GET['tab'])){
 					ORDER BY fixProfitID, fixItemID";
 			
 	$rs =$oSQL->q($sql);
-	
-	?>
-	<div id="report">
-	<pre style="display:none;"><?php echo $sql;?></pre>
-	<ol>
-	<?php
 	while ($rw=$oSQL->f($rs)){
 		// print_r($rw);
 		echo "<li>[{$rw['fixID']}] {$rw["fixTitle"]}, {$rw["pccTitle$strLocal"]}
+			<div><small>".$rw['fixDeprStart']." &ndash; ".$rw['fixDeprEnd']."</small></div>
+			</li>";
+	}
+	?>
+	</ol>
+	<h2>Misplaced assets</h2>
+	<ol>
+	<?php
+	$sql = "SELECT vw_fixed_assets.*, pcFix.pccTitle as fixProfit, pcDep.pccTitle as depProfit 
+						FROM reg_depreciation
+						LEFT JOIN vw_fixed_assets ON particulars=fixGUID
+						LEFT JOIN vw_profit pcFix ON pcFix.pccID=fixProfitID
+						LEFT JOIN vw_profit pcDep ON pcDep.pccID=pc
+						WHERE scenario='{$oBudget->id}' 
+							AND fixProfitID<>pc
+						ORDER BY fixProfitID, fixItemID";
+			
+	$rs =$oSQL->q($sql);
+	while ($rw=$oSQL->f($rs)){
+		// print_r($rw);
+		echo "<li>[{$rw['fixID']}] {$rw["fixTitle"]}, {$rw["fixProfit"]} registered in {$rw["depProfit"]}
 			<div><small>".$rw['fixDeprStart']." &ndash; ".$rw['fixDeprEnd']."</small></div>
 			</li>";
 	}
