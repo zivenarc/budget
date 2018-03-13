31e69cd3639267326b46ba29aaef8c28ffcf15de
Joseph Topotushko <jt@usa.net>
2018-03-13 11:04:36 +0300

    Bunticks

--- sp_repost_hq.php
+++ sp_repost_hq.php
@@ -1,9 +1,9 @@
 <?php
-$flagNoAuth =true;
+// $flagNoAuth =true;
 require ('common/auth.php');
 
 if ($_GET['tab']){
-	
+	$budget_scenario = $_GET['tab'];
 	?>
 <button onclick="repost('<?php echo $_GET['tab']; ?>', event);">Repost documents</button>
 <div/>
@@ -12,7 +12,7 @@ if ($_GET['tab']){
 	require ('classes/reports.class.php');
 	$sql = "SELECT *, edit_date as timestamp FROM vw_journal 				
 		LEFT JOIN stbl_user ON usrID=vw_journal.edit_by				
-		WHERE vw_journal.scenario='{$_GET['tab']}' 
+		WHERE vw_journal.scenario='{$budget_scenario}' 
 		AND prefix IN ('msf')
 		GROUP BY vw_journal.guid
 		ORDER BY vw_journal.edit_date ASC";	
@@ -22,11 +22,14 @@ if ($_GET['tab']){
 			$data[] = $rw;
 		}
 		
-		echo "<div id='div_{$_GET['tab']}'>";
+		echo "<div id='div_{$budget_scenario}'>";
 		Reports::getJournalEntries($data);
 		echo '</div>';
-		
-	
+		?>
+		<nav>
+		<a href="rep_totals.php?<?php echo $strQuery;?>&budget_scenario=<?php echo $budget_scenario;?>&bu_group=000000014">Results per BU</a>
+		</nav>
+		<?php
 } else {
 	require ('classes/budget.class.php');
 	$arrJS[] = 'js/journal.js';		
