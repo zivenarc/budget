b719b2e626e0721af08e7d78cbd24cd603561293
Joseph Topotushko <jt@usa.net>
2018-04-10 11:17:41 +0300

    DM

--- employee_current_form.php
+++ employee_current_form.php
@@ -35,7 +35,7 @@ if ($_POST['DataAction']){
 				LEFT JOIN common_db.tbl_employee ON bnsEmployeeID=empID
 				LEFT JOIN treasury.tbl_bonus_period ON bpdID=bnsPeriodID
 				WHERE bnsPeriodID=(SELECT MAX(bpdID) FROM treasury.tbl_bonus_period WHERE bpdFlagSalaryReview=1)
-				AND bnsStateID BETWEEN 2240 AND 2279"
+				AND bnsStateID BETWEEN 2240 AND 2279";
 				// AND empProfitID='{$oDocument->profit}'";
 				
 		$rs = $oSQL->q($sql);
