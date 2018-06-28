4224fe34ee65ca95f9b6c08065e30798eadd5998
Joseph Topotushko <jt@usa.net>
2018-06-28 14:42:01 +0300

    Bunticks

--- includes/inc_journals.php
+++ includes/inc_journals.php
@@ -1,7 +1,7 @@
 		<div class='panel' id='journal-user' style='float:right;'>
 			<nav>
 			<?php
-			$sql = "SELECT responsible, usrTitle, SUM(1-posted-deleted) as nCount
+			$sql = "SELECT responsible, usrTitle$strLocal as usrTitle, usrFlagDeleted, SUM(1-posted-deleted) as nCount
 					FROM vw_journal 
 					LEFT JOIN stbl_user ON responsible=usrID
 					WHERE scenario='{$_GET['tab']}'
@@ -10,7 +10,7 @@
 			$rs =$oSQL->q($sql);
 			while ($rw=$oSQL->f($rs)){
 				?>
-				<div><a href='sp_my.php?ownerID=<?php echo $rw['responsible'];?>#<?php echo $_GET['tab'];?>'><?php echo $rw['usrTitle'],($rw['nCount']?(' ('.$rw['nCount'].')'):'');?></a></div>
+				<div><a href='sp_my.php?ownerID=<?php echo $rw['responsible'];?>#<?php echo $_GET['tab'];?>'><?php echo ($rw['usrFlagDeleted']?'<del>':''),$rw['usrTitle'],($rw['usrFlagDeleted']?'</del>':''),($rw['nCount']?(' ('.$rw['nCount'].')'):'');?></a></div>
 				<?php
 			}
 			?>
@@ -19,7 +19,7 @@
 		<div class='panel' id='journal-bu' style='float:right;'>
 			<nav>
 			<?php
-			$sql = "SELECT pc, pccTitle, SUM(1-posted-deleted) as nCount
+			$sql = "SELECT pc, pccTitle$strLocal as pccTitle, pccFlagDeleted, SUM(1-posted-deleted) as nCount
 					FROM vw_journal 
 					JOIN common_db.tbl_profit ON pc=pccID
 					WHERE scenario='{$_GET['tab']}'
@@ -28,7 +28,7 @@
 			$rs =$oSQL->q($sql);
 			while ($rw=$oSQL->f($rs)){
 				?>
-				<div><a href='sp_bu.php?pc=<?php echo $rw['pc'];?>#<?php echo $_GET['tab'];?>'><?php echo $rw['pccTitle'],' (',$rw['nCount'],')';?></a></div>
+				<div><a href='sp_bu.php?pc=<?php echo $rw['pc'];?>#<?php echo $_GET['tab'];?>'><?php echo ($rw['pccFlagDeleted']?'<del>':''),$rw['pccTitle'],($rw['pccFlagDeleted']?'</del>':''),' (',$rw['nCount'],')';?></a></div>
 				<?php
 			}
 			?>
