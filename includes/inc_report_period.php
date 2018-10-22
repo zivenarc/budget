a717ab2d155931e7108e0cf039fd65e6ef49a449
Joseph Topotushko <jt@usa.net>
2018-10-22 14:42:14 +0300

    Bunticks

---
+++ includes/inc_report_period.php
@@ -0,0 +1,15 @@
+<?php
+$arrPeriodType = Array();
+$arrPeriodType['ytd'] = 'YTD';
+$arrPeriodType['cm'] = 'Current month';
+$arrPeriodType['nm'] = 'Next month';
+$arrPeriodType['q1'] = '1<sup>st</sup> quarter';
+$arrPeriodType['q2'] = '2<sup>nd</sup> quarter';
+$arrPeriodType['q3'] = '3<sup>rd</sup> quarter';
+$arrPeriodType['q4'] = '4<sup>th</sup> quarter';
+$arrPeriodType['q5'] = '5<sup>th</sup> quarter';
+$arrPeriodType['roy'] = 'Rest-of-year';
+$arrPeriodType['fye'] = 'FYE';
+$arrPeriodType['am'] = 'Apr-Mar';
+
+?>
