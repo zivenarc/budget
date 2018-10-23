90432734e6985728f874f228bae6fc383f0ba73d
Joseph Topotushko <jt@usa.net>
2018-10-23 10:43:33 +0300

    Bunticks

--- rep_admin_costs.php
+++ rep_admin_costs.php
@@ -1,16 +1,11 @@
 <?php
 //$flagNoAuth = true;
 require ('common/auth.php');
-require ('classes/budget.class.php');
 require ('classes/reports.class.php');
 require ('classes/reference.class.php');
 require ('classes/item.class.php');
-
-$oBudget = new Budget($budget_scenario);
+include ('includes/inc_report_settings.php');
 $arrJS[]='js/rep_pnl.js';
-
-include ('includes/inc-frame_top.php');
-echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
 
 $arrItems[] = Items::COMPANY_CARS;
 $arrItems[] = Items::OFFICE_RENT;
@@ -21,36 +16,17 @@ $arrItems[] = Items::EXPAT_HOUSING;
 $arrItems[] = Items::EXPAT_COSTS;
 $arrItems[] = Items::OTHER_COMMUNICATION;
 
+$oReport = new Reports(Array('budget_scenario'=>$budget_scenario, 'currency'=>$currency, 'denominator'=>$denominator, 'reference'=>$reference,'filter'=>Array('item'=>$arrItems)));
+$oBudget = new Budget($budget_scenario);
 
-$strItems = "'".implode("','",$arrItems)."'";
+include ('includes/inc-frame_top.php');
+echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
 
-$sqlWhere = "WHERE `item` IN ($strItems) AND scenario='$budget_scenario' ";
+include ('includes/inc_report_selectors.php');
 ?>
-<script>
-	$(document).ready(function(){
-		$('#period_switch').buttonset().children('input').change(function(){
-				if($(this).val()=='monthly'){
-					$('.budget-monthly').show();
-					$('.budget-quarterly').hide();
-				} else {
-					$('.budget-monthly').hide();
-					$('.budget-quarterly').show();
-				}
-		});
-		init_panel($('#report_content'));
-	});
-</script>
-<div class="report-radio f-row">
-<div id='period_switch'>
-		<input type='radio' id='period_monthly_<?php echo $_GET['tab'];?>' name='period' value='monthly'/>
-		<label for='period_monthly_<?php echo $_GET['tab'];?>'>Monthly</label>
-		<input type='radio' checked id='period_quarterly_<?php echo $_GET['tab'];?>' name='period' value='quarterly'/>
-		<label for='period_quarterly_<?php echo $_GET['tab'];?>'>Quarterly</label>
-	</div>
-</div>
 <div id='report_content'>
 <?php
-Reports::masterbyProfitEst($sqlWhere);
+$oReport->quarterly('item_bu',false);	
 ?>
 </div>
 <?php
--- rep_it_costs.php
+++ rep_it_costs.php
@@ -1,11 +1,11 @@
 <?php
 //$flagNoAuth = true;
 require ('common/auth.php');
-require ('classes/budget.class.php');
+require ('classes/reports.class.php');
 require ('classes/reference.class.php');
 require ('classes/item.class.php');
-require ('classes/reports.class.php');
 include ('includes/inc_report_settings.php');
+$arrJS[]='js/rep_pnl.js';
 
 $arrItems[] = Items::IT_EQUIPMENT;
 $arrItems[] = Items::INTERNET;
@@ -17,19 +17,11 @@ $arrItems[] = Items::RHQ_IT_COST;
 
 $oReport = new Reports(Array('budget_scenario'=>$budget_scenario, 'currency'=>$currency, 'denominator'=>$denominator, 'reference'=>$reference,'filter'=>Array('item'=>$arrItems)));
 $oBudget = new Budget($budget_scenario);
-$arrJS[]='js/rep_pnl.js';
 
 include ('includes/inc-frame_top.php');
 echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,'</h1>';
 
 include ('includes/inc_report_selectors.php');
-// include ('includes/inc_report_buttons.php');
-
-
-
-$strItems = "'".implode("','",$arrItems)."'";
-
-$sqlWhere = "WHERE `item` IN ($strItems)";
 ?>
 <div id='report_content'>
 <?php
