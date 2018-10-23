aef7af2953fca31c24df1fdc1ea15d6b1d944809
Joseph Topotushko <jt@usa.net>
2018-10-23 15:27:37 +0300

    new business

---
+++ rep_pnl_new.php
@@ -0,0 +1,42 @@
+<?php
+require ('common/auth.php');
+require ('classes/budget.class.php');
+
+include ('includes/inc_report_settings.php');
+include ('includes/inc_report_pcfilter.php');
+
+if(!isset($_GET['pccGUID'])){
+	$oBudget = new Budget($budget_scenario);
+	// if ($reference!=$oBudget->reference_scenario->id){
+		// $oReference = new Budget($reference);
+		// $strVsTitle = ' vs '.$oReference->title;
+	// } else {
+		// $reference = $oBudget->reference_scenario->id;
+	// }
+	
+	$arrJS[]='js/rep_pnl.js';
+	// $arrJS[]='js/input_form.js';	
+	
+	include('includes/inc_group_buttons.php');	
+	include ('includes/inc-frame_top.php');
+	echo '<h1>',$arrUsrData["pagTitle$strLocal"],': ',$oBudget->title,$strVsTitle,'</h1>';
+	echo '<h2>Reference to actual periods not available</h2>';
+	include ('includes/inc_report_selectors.php');
+	echo '<p>',$oBudget->timestamp,'; ',$oBudget->rates,'</p>';
+	
+	Budget::getProfitTabs('reg_master', true, Array('pccID'=>$arrBus));
+	
+	include ('includes/inc-frame_bottom.php');
+} else {
+	require ('classes/reports.class.php');
+	include ('includes/inc_report_buttons.php');
+	
+	$filter['new'] = true;
+	
+	$oReport = new Reports(Array('budget_scenario'=>$budget_scenario, 'currency'=>$currency, 'denominator'=>$denominator,'reference'=>$budget_scenario,'filter'=>$filter));
+	// echo '<pre>';print_r($filter);echo '</pre>';
+	$oReport->periodicPnL($type);
+}
+
+
+?>
