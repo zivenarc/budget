a00cec08a2488adb2e3ca2bc868668ade032cde0
Joseph Topotushko <jt@usa.net>
2018-11-27 19:35:03 +0300

    Bunticks

--- sp_repost_salesrhq.php
+++ sp_repost_salesrhq.php
@@ -11,23 +11,32 @@ if ($_GET['tab']){
 	<?php
 	require ('classes/reports.class.php');
 	
-	$sql = "SELECT source, salID, salAmount, SUM(apr+may+jun+jul+aug+sep+`oct`+`nov`+`dec`+jan_1+feb_1+mar_1) as Total_RHQ
-			FROM reg_sales_rhq 			
-			LEFT JOIN tbl_sales ON salGUID=source 
+	$sql = "SELECT source SUM(Total_AM) as Total_AM
+			FROM vw_master 			
 			WHERE scenario = '{$_GET['tab']}'
+			AND account IN ('J00400', 'J00802','J45010','J40010')
 			GROUP BY source 
-			HAVING Total_RHQ<>salAmount";
+			";
 	$rs = $oSQL->q($sql);
 	while ($rw = $oSQL->f($rs)){
 		$arrGUID[] = $rw['source'];
+		$arrAM[$rw['source']] = $rw['Total_AM'];
+	}
+	
+	$sql = "SELECT source SUM(Apr+May+Jun+Jul+Aug+Sep+`Oct`+Nov+`Dec`+Jan_1+Feb_1+Mar_1) as Total_RHQ
+			FROM reg_sales_rhq 			
+			WHERE scenario = '{$_GET['tab']}'			
+			GROUP BY source";
+	while ($rw = $oSQL->f($rs)){		
 		$arrRHQ[$rw['source']] = $rw['Total_RHQ'];
 	}
+	
 	
 	$sql = "SELECT *,  edit_date as timestamp FROM vw_journal 				
 		LEFT JOIN stbl_user ON usrID=vw_journal.edit_by				
 		WHERE vw_journal.posted=1 
 		AND vw_journal.scenario='{$_GET['tab']}' 
-		AND prefix IN ('sal', 'ics')
+		AND prefix IN ('sal', 'ics', 'cst')
 		AND vw_journal.guid IN ('".implode("','",$arrGUID)."')
 		GROUP BY vw_journal.guid
 		ORDER BY vw_journal.edit_date ASC";	
