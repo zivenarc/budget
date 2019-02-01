5f07f39ee693c1dcf63feadaf3fe946afc3bf47b
Joseph Topotushko <jt@usa.net>
2019-02-01 13:47:48 +0300

    Ololo

--- classes/reports.class.php
+++ classes/reports.class.php
@@ -503,7 +503,7 @@ class Reports{
 				};
 				if($rw['freehand']) {
 					?>
-					<td><a href="javascript:getCustomerKPI({filter:{prtGHQ:'<?php echo $rw['prtGHQ'];?>',route:<?php echo $rw['route'];?> ,freehand:1}});"><?php echo $rw['rteTitle'];?></a></td>								
+					<td><a href="javascript:getCustomerKPI({filter:{prtGHQ:'<?php echo $rw['prtGHQ'];?>',route:<?php echo $rw['route'];?> ,freehand:1,activity:<?php echo json_encode($activity_filter);?>}});"><?php echo $rw['rteTitle'];?></a></td>								
 					<?php
 				}
 				for ($m=1+$this->oBudget->offset;$m<=12+$this->oBudget->offset;$m++){
