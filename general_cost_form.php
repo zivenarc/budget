f733a343ab91e68f2f4071b1986791bc669113dd
Joseph Topotushko <jt@usa.net>
2019-05-20 16:56:06 +0300

    Выгрузка в Excel

--- general_cost_form.php
+++ general_cost_form.php
@@ -11,6 +11,41 @@ $oDocument->defineEF();
 $oDocument->defineGrid();
 $oBudget = new Budget($oDocument->scenario);
 
+if ($_GET['DataAction']=='excel'){	
+	include_once ("../common/eiseList/inc_excelXML.php");
+	$xl = new excelXML();            
+	$arrHeader = Array('Организация','Подразделение','Номенклатура','Номенклатурная группа','Содержание','Количество','Цена','Сумма');		
+	$xl->addHeader($arrHeader);
+	
+	$sql = "SELECT comID, comTitleLocal, pc, pccTitleLocal, product, prdTitleLocal,activity,prtTitleLocal, buying_rate, ".$oBudget->getThisYTDSQL('nm')." as nQuantity
+			FROM reg_costs	
+			LEFT JOIN common_db.tbl_company ON comID=company
+			LEFT JOIN common_db.tbl_profit ON pccID=pc
+			LEFT JOIN common_db.tbl_product ON prdID=product
+			LEFT JOIN common_db.tbl_product_type ON prtID=activity
+			WHERE source='{$oDocument->GUID}'
+			";
+			
+	
+	$rs = $oSQL->q($sql);
+	while ($rw = $oSQL->f($rs)){
+	
+			$arrRow = Array();
+			$arrRow[] = $rw['comTitleLocal'];	
+			$arrRow[] = $rw['pccTitleLocal'];					
+			$arrRow[] = $rw['prdTitleLocal'];					
+			$arrRow[] = $rw['prtTitleLocal'];					
+			$arrRow[] = $rw['comment'];					
+			$arrRow[] = number_format($rw['nQuantity'],3,'.','');	
+			$arrRow[] = number_format($rw['buying_rate'],3,'.','');					
+			$xl->addRow($arrRow);
+	}
+     
+	$xl->Output($oDocument->GUID);
+	die();	
+}
+
+
 if ($_POST['DataAction']){
 	 // echo '<pre>'; print_r($_POST);	 echo '</pre>';
 	if(strpos($_POST['DataAction'],'fill_')!==false){
@@ -50,20 +85,23 @@ if ($_GET['tab']){
 
 
 include ('includes/inc_document_menu.php');
-if ($oDocument->GUID && !$oDocument->flagPosted){
-	$arrActions[] = Array ('title'=>'Headcount','action'=>'javascript:fillGrid(\'_hc\');','class'=>'brick');
-	$arrActions[] = Array ('title'=>'Blue collars','action'=>'javascript:fillGrid(\'_bc\');','class'=>'user_blue');
-	$arrActions[] = Array ('title'=>'Users','action'=>'javascript:fillGrid(\'_users\');','class'=>'user_suit');
-	$arrActions[] = Array ('title'=>'TEU','action'=>'javascript:fillGrid(\'_teu\');','class'=>'bricks');
-	$arrActions[] = Array ('title'=>'Revenue','action'=>'javascript:fillGrid(\'_revenue\');','class'=>'money');
-	$arrActions[] = Array ('title'=>'Payroll','action'=>'javascript:fillGrid(\'_payroll\');','class'=>'fa-users');
-	$arrActions[] = Array ('title'=>'Air import','action'=>'javascript:fillGrid(\'_ai\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'Air export','action'=>'javascript:fillGrid(\'_ae\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'Ocean import','action'=>'javascript:fillGrid(\'_oi\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'Ocean export','action'=>'javascript:fillGrid(\'_oe\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'OCM','action'=>'javascript:fillGrid(\'_ocm\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'LT','action'=>'javascript:fillGrid(\'_lt\');','class'=>'hidden');
-	$arrActions[] = Array ('title'=>'CL','action'=>'javascript:fillGrid(\'_wh\');','class'=>'hidden');
+if ($oDocument->GUID){
+	if (!$oDocument->flagPosted){
+		$arrActions[] = Array ('title'=>'Headcount','action'=>'javascript:fillGrid(\'_hc\');','class'=>'brick');
+		$arrActions[] = Array ('title'=>'Blue collars','action'=>'javascript:fillGrid(\'_bc\');','class'=>'user_blue');
+		$arrActions[] = Array ('title'=>'Users','action'=>'javascript:fillGrid(\'_users\');','class'=>'user_suit');
+		$arrActions[] = Array ('title'=>'TEU','action'=>'javascript:fillGrid(\'_teu\');','class'=>'bricks');
+		$arrActions[] = Array ('title'=>'Revenue','action'=>'javascript:fillGrid(\'_revenue\');','class'=>'money');
+		$arrActions[] = Array ('title'=>'Payroll','action'=>'javascript:fillGrid(\'_payroll\');','class'=>'fa-users');
+		$arrActions[] = Array ('title'=>'Air import','action'=>'javascript:fillGrid(\'_ai\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'Air export','action'=>'javascript:fillGrid(\'_ae\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'Ocean import','action'=>'javascript:fillGrid(\'_oi\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'Ocean export','action'=>'javascript:fillGrid(\'_oe\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'OCM','action'=>'javascript:fillGrid(\'_ocm\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'LT','action'=>'javascript:fillGrid(\'_lt\');','class'=>'hidden');
+		$arrActions[] = Array ('title'=>'CL','action'=>'javascript:fillGrid(\'_wh\');','class'=>'hidden');
+	}
+	$arrActions[] = Array ('title'=>'Excel','action'=>$_SERVER['REQUEST_URI'].'&DataAction=excel','class'=>'fa fa-excel hidden');
 }
 
 //============================== Main form definition ==============================
